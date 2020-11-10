99dbd510496b330c83b2a4bd6c73a93c9ed0222c
mareta103 <74005509+mareta103@users.noreply.github.com>
2020-11-10 20:15:48 +0700

    Update Python.py

--- Python.py
+++ Python.py
@@ -1,2 +1,9 @@
 # Pseudocode-Algoritma-Mareta-dan-Triana
 # Pseudocode-Python
+
+a = int(input("Masukkan a : "))
+b = int(input("Masukkan b : "))
+X = a + b 
+Y = X * X
+print ("hasil kuadrat :",Y)
+
