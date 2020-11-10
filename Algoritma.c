d4487148b884b378dd1cdc3a4cbd50deb5a9e79c
trianasalsabila <73945435+trianasalsabila@users.noreply.github.com>
2020-11-10 18:49:50 +0700

    Update and rename README.md to Algoritma.c

---
+++ Algoritma.c
@@ -0,0 +1,40 @@
+# Pseudocode-Algoritma-Mareta-dan-Triana
+# Pseudocode-Bahasa C
+
+
+Nomer 2
+#include <stdio.h>
+#include <math.h>
+void main()
+{
+    int a,b,hasil;
+
+    printf("Masukkah nilai A : ");
+    scanf("%d", &a);
+    printf("\n");
+    printf("Masukkan nilai B : ");
+    scanf("%d", &b);
+    printf("\n");
+    hasil = a + b;
+    hasil = hasil * hasil;
+    printf("hasil adalah %d", hasil);
+}
+
+
+Nomer 3
+#include <stdio.h>
+void main()
+{
+    int a,b,c,hasil;
+
+    printf("Masukkan nilai A : ");
+    scanf("%d", &a);
+    printf("Masukkan nilai B : ");
+    scanf("%d", &b);
+    printf("\n");
+    printf("Masukkan nilai C : ");
+    scanf("%d", &c);
+    printf("\n");
+    hasil = a *b * c;
+    printf("Hasil adalah %d", hasil);
+}
--- README.md
+++
@@ -1,1 +0,0 @@
-# Pseudocode-Algoritma-Mareta-dan-Triana
